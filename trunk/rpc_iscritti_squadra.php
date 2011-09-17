1fe7f4b392d3f89b8d4b440bce61390cd6c807d0
lucio.fassio@gmail.com <lucio.fassio@gmail.com@85a41e8a-0e26-f383-f923-33260acbc95f>
2011-09-17 15:59:39 +0000

    Visualizzato numero di iscritti per squadra

--- trunk/rpc_iscritti_squadra.php
+++ trunk/rpc_iscritti_squadra.php
@@ -7,17 +7,26 @@ if(isset($_POST['IDSquadra']) && isset($_POST['IDEvento'])) {
 	// contro sql injection
 	$IDSquadra = mysql_real_escape_string($_POST['IDSquadra']);
 	$IDEvento = mysql_real_escape_string($_POST['IDEvento']);
+	//$IDSquadra = mysql_real_escape_string($_GET['IDSquadra']);
+	//$IDEvento = mysql_real_escape_string($_GET['IDEvento']);
 	if(strlen($IDSquadra) > 0 && strlen($IDEvento) > 0) {
-		$query = mysql_query("SELECT Catechismi.ID, Cognome, Nome FROM tblIscrizioni INNER JOIN Catechismi ON tblIscrizioni.ID = Catechismi.ID WHERE tblIscrizioni.IDSquadra = ".$IDSquadra." AND tblIscrizioni.IdEvento=".$IDEvento." ORDER BY Cognome, Nome;");
+		$sql = "SELECT Catechismi.ID, Cognome, Nome FROM tblIscrizioni INNER JOIN Catechismi ON tblIscrizioni.ID = Catechismi.ID WHERE tblIscrizioni.IDSquadra = %1\$s AND tblIscrizioni.IdEvento= %2\$s ORDER BY Cognome, Nome;";
+		$sql = sprintf($sql, $IDSquadra, $IDEvento);
+		$query = mysql_query($sql);
 		if (mysql_errno() == 0) {
-			while ($row = mysql_fetch_object($query)) {
-    			echo "<option value=".$row->ID." class=\"listaiscritti\">".htmlentities($row->Cognome)." ".htmlentities($row->Nome)."</option>";
+			if (! mysql_num_rows($query)) {
+				echo "0|";	
+			} else {
+				echo mysql_num_rows($query)."|";
+				while ($row = mysql_fetch_object($query)) {
+					echo "<option value=".$row->ID." class=\"listaiscritti\">".htmlentities($row->Cognome)." ".htmlentities($row->Nome)."</option>";
+				}
 			}
 		} else {
 			echo("rpc_iscritti_squadra: ".mysql_errno().":".mysql_error()."<br/><br/>".$sql);
 			exit();
 		}  
-	} // There is a queryString.
+	} // nessun dato in input
 } else {
 	echo 'Questa pagina non deve essere acceduta direttamente.';
 }
